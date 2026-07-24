810af6620d06c6cd30b3f6895415b449445e2427
HP <sheetalhir30@gmail.com>
2026-07-24 13:19:16 +0530

    Update jest.config.ts and package.json

--- bugtracker-frontend/jest.config.ts
+++ bugtracker-frontend/jest.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "jest";
-import nextJest from "next/jest";
+import nextJest from "next/jest.js";  // note the .js extension
 
 const createJestConfig = nextJest({
   dir: "./",
